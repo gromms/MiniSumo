168d949814ea99da2a95c6929b6217f5905461aa
Simmo Saan <simmo.saan@gmail.com>
2016-05-26 19:49:00 +0300

    Implement basic slow sumo logic

--- MiniSumo/minisumo.c
+++ MiniSumo/minisumo.c
@@ -50,37 +50,35 @@ int main(void)
 		bool lightr = bit_get(PIND, BIT(LIGHTR));
 		bool btn = !bit_get(PINB, BIT(START_BTN));
 
-		/*if (distl && !distr)
+		if (lightl || lightr)
 		{
-			usart_write("1:s0.5\n");
-			usart_write("2:s-0.8\n");
-		}
-		else if (!distl && distr)
-		{
-			usart_write("1:s0.8\n");
-			usart_write("2:s-0.5\n");
-		}
-		else if (distm)
-		{
-			usart_write("1:s1.2\n");
-			usart_write("2:s-1.2\n");
+			usart_write(lightl ? "1:s0.1\n" : "1:s0\n");
+			usart_write(lightr ? "2:s0.1\n" : "2:s0\n");
 		}
 		else
 		{
-			usart_write("1:s0\n");
-			usart_write("2:s0\n");
-		}*/
+			if (distl && !distr)
+			{
+				usart_write("1:s-0.1\n");
+				usart_write("2:s-0.1\n");
+			}
+			else if (!distl && distr)
+			{
+				usart_write("1:s-0.1\n");
+				usart_write("2:s0\n");
+			}
+			else if (distm)
+			{
+				usart_write("1:s0\n");
+				usart_write("2:s-0.1\n");
+			}
+			else
+			{
+				usart_write("1:s0.1\n");
+				usart_write("2:s-0.1\n");
+			}
+		}
 
-		/*if (lightl)
-			usart_write("aaa\n");
-		else if (lightr)
-			usart_write("ggg\n");
-		else
-			usart_write("xxx\n");*/
-
-		usart_write(lightl ? "1:s0.3\n" : "1:s0\n");
-		usart_write(lightr ? "2:s0.3\n" : "2:s0\n");
-
-		_delay_ms(100);
+		_delay_ms(10);
 	}
 }
